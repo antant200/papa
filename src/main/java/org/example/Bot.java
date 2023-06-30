1d2f0de63a9f73889b87f1b3bc99e6a17b1fc31e
antant200 <antonmarkelov20020605@yandex.ru>
2023-06-30 22:56:21 +0700

    init

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -4,25 +4,28 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.math.BigDecimal;
+;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Math.floor;
+import static java.lang.Math.ceil;
 import static java.math.RoundingMode.HALF_UP;
 
 
 public class Bot extends TelegramLongPollingBot {
     String operation;
+    String operationMoney;
+    boolean check;
 
     String nameMoney;
-
+    int money;
     @Override
     public void onUpdateReceived(Update update) {
         try {
@@ -33,6 +36,7 @@ public class Bot extends TelegramLongPollingBot {
                 SendMessage outMessage = new SendMessage(); // наш ответ пользователю
                 outMessage.setChatId(chatId);
                 outMessage.setText(response);
+                outMessage.setParseMode("HTML");
                 // Создаем кнопки "Лонг" и "Шорт"
                 if (message.getText().equals("/start")) {
                     outMessage.setReplyMarkup(getOperationKeyboard());
@@ -47,101 +51,122 @@ public class Bot extends TelegramLongPollingBot {
     public String parseMess(String textMess) {
         String response = null;
         if (textMess.equals("/start")) {
-            response = "Введите какую позицию: Лонг или Шорт?";
+            response = "Бот умеет \n 1) Считать позиции (Лонг/Шорт)";
         } else if (textMess.equals("Лонг") || textMess.equals("Шорт")) {
-            operation = textMess;
+            check=false;
+            response = "Введите размер депозита (без плеча)";
+            operation=textMess;
+            operationMoney = "депозит";
+        }  else if (!check && operationMoney.equals("депозит")){
+            money = Integer.parseInt(textMess)*2;
+            operationMoney=null;
+            check=true;
             response = "Введите название монеты:";
-        }
-        else if (textMess.matches("[A-Za-z]+")) {
+        } else if (textMess.matches("[A-Za-z]+")) {
             nameMoney = textMess;
             response = "Введите точку входа:";
         } else if (isNumeric(textMess)) {
             double entryPoint = Double.parseDouble(textMess);
 
             int amount1, amount2, amount3;
-            double cost2, cost3, averageCost, averageCost2, exitPrice, exitPrice2, profit, profit2;
+            double cost2, cost3, averageCost, averageCost2,exitPrice, exitPrice2, profit, profit2, exitPrice1,profit1;
             if (operation.equals("Лонг")) {
 
-            amount1 = (int) Math.floor(600 / entryPoint);
-
-            cost2 = entryPoint - 0.05 * entryPoint;
-            BigDecimal resultCost2 = new BigDecimal(cost2);
-            resultCost2 = resultCost2.setScale(4, BigDecimal.ROUND_HALF_UP);
-            averageCost = (cost2 + entryPoint) / 2;
-            BigDecimal resultAverageCost = new BigDecimal(averageCost);
-            resultAverageCost = resultAverageCost.setScale(4, BigDecimal.ROUND_HALF_UP);
-            amount2 = (int) Math.floor(600 / cost2);
-            exitPrice = 1.05 * averageCost;
-            BigDecimal resultExitPrice = new BigDecimal(exitPrice);
-            resultExitPrice = resultExitPrice.setScale(4, HALF_UP);
-            profit = -(amount1 * entryPoint + amount2 * cost2) + ((amount2 + amount1) * exitPrice);
-            BigDecimal resultProfit = new BigDecimal(profit);
-            resultProfit = resultProfit.setScale(0, HALF_UP);
+                amount1 = (int) Math.ceil((money / entryPoint) * 0.3);
+                exitPrice1 = 1.03 * entryPoint;
+                BigDecimal resultExitPrice1 = new BigDecimal(exitPrice1).setScale(4,HALF_UP);
+                profit1 = ((exitPrice1 - entryPoint) * amount1);
+                BigDecimal resultProfit1 = new BigDecimal(profit1).setScale(1, HALF_UP);
 
 
-            cost3 = entryPoint - 0.1 * entryPoint;
-            BigDecimal resultCost3 = new BigDecimal(cost3);
-            resultCost3 = resultCost3.setScale(4, HALF_UP);
-            averageCost2 = (cost3 + cost2 + entryPoint) / 3;
-            BigDecimal resultAverageCost2 = new BigDecimal(averageCost2);
-            resultAverageCost2 = resultAverageCost2.setScale(4, HALF_UP);
-            amount3 = (int) floor(800 / cost3);
-            exitPrice2 = 1.1 * averageCost2;
-            BigDecimal resultExitPrice2 = new BigDecimal(exitPrice2);
-            resultExitPrice2 = resultExitPrice2.setScale(4, HALF_UP);
-            profit2 = -(amount1 * entryPoint + amount2 * cost2 + amount3 * cost3) + ((amount3 + amount1 + amount2) * exitPrice);
-            BigDecimal resultProfit2 = new BigDecimal(profit2);
-            resultProfit2 = resultProfit2.setScale(0, HALF_UP);
-            response = ("Монета: " + nameMoney + " (Лонг) " + "\n" +
-                    "1 вход. Кол-во -- " + amount1 + "; Цена покупки: " + entryPoint + "\n" + "\n"
-                    + "2 вход. Кол-во -- " + amount2 + "; Цена покупки: " + resultCost2 + "; Средняя цена: " + resultAverageCost + "\n"
-                    + "Цена выхода (5%): " + resultExitPrice + "; Сумма прибыли: " + resultProfit + "\n" + "\n"
-                    + "3 вход. Кол-во -- " + amount3 + "; Цена покупки: " + resultCost3 + "; Средняя цена: " + resultAverageCost2 + "\n"
-                    + "Цена выхода (10%): " + resultExitPrice2 + "; Сумма прибыли: " + resultProfit2);}
-            else if (operation.equals("Шорт")) {
-                amount1 = (int) Math.floor(480 / entryPoint);
-
-                cost2 = entryPoint + 0.05 * entryPoint;
-                BigDecimal resultCost2 = new BigDecimal(cost2);
-                resultCost2 = resultCost2.setScale(4, BigDecimal.ROUND_HALF_UP);
+                cost2 = 0.95 * entryPoint;
+                BigDecimal resultCost2 = new BigDecimal(cost2).setScale(4, HALF_UP);
                 averageCost = (cost2 + entryPoint) / 2;
-                BigDecimal resultAverageCost = new BigDecimal(averageCost);
-                resultAverageCost = resultAverageCost.setScale(4, BigDecimal.ROUND_HALF_UP);
+                BigDecimal resultAverageCost = new BigDecimal(averageCost).setScale(4, HALF_UP);
+                amount2 = amount1;
+                exitPrice = 1.05*averageCost;
+                BigDecimal resultExitPrice =  new BigDecimal(exitPrice).setScale(4,HALF_UP);
+                profit = (exitPrice-averageCost) *(amount1+amount2);
+                BigDecimal resultProfit = new BigDecimal(profit).setScale(1, HALF_UP);
 
-                amount2 = (int) Math.floor(480 / cost2);
+
+                cost3 = 0.9 * entryPoint;
+                BigDecimal resultCost3 = new BigDecimal(cost3).setScale(4, HALF_UP);
+                amount3 = (int) ceil((money/ entryPoint) * 0.4);
+                averageCost2 = (amount3 * cost3 + amount2 * cost2 + amount1 * entryPoint) / (amount1 + amount2 + amount3);
+                BigDecimal resultAverageCost2 = new BigDecimal(averageCost2).setScale(4, HALF_UP);
+                exitPrice2 = 1.1 * averageCost2;
+                BigDecimal resultExitPrice2 = new BigDecimal(exitPrice2).setScale(4, HALF_UP);
+                profit2 = (exitPrice2 - averageCost2) * (amount1 + amount2 + amount3);
+                BigDecimal resultProfit2 = new BigDecimal(profit2).setScale(1, HALF_UP);
+
+
+                response = ("<b> Монета: " + nameMoney + " </b> (Лонг) " + "\n" +
+                        "\uD83D\uDFE2<b>Точка  входа 1.  --> </b> " + entryPoint +
+                        "\n<b>Размер позиции: </b>" + amount1  + "\n" +
+                        "<b>Точка выхода (3%): </b>" + resultExitPrice1 + "\n" + "\n"
+                        + "\uD83D\uDFE2 \uD83D\uDFE2  <b> Точка входа 2. </b> " + resultCost2 +
+                        "\n<b>Размер позиции: </b>" + amount2 +  "\n"
+                        + "<b>Точка выхода (5%): </b>" + resultExitPrice + "\n" + "\n"
+                        + "\uD83D\uDFE2 \uD83D\uDFE2 \uD83D\uDFE2  <b> Точка входа 3.</b>" + resultCost3+
+                        "\n<b>Размер позиции: </b>" + amount3 +  "\n"
+                        + "<b>Точка выхода (10%): </b>" + resultExitPrice2);
+
+            } else if (operation.equals("Шорт")) {
+                amount1 = (int) Math.ceil((money / entryPoint) * 0.3);
+                exitPrice1 = 1.03 * entryPoint;
+                BigDecimal resultExitPrice1 = new BigDecimal(exitPrice1).setScale(4, HALF_UP);
+                profit1 = (int) ((exitPrice1 - entryPoint) * amount1);
+
+                cost2 = 1.05 * entryPoint;
+                BigDecimal resultCost2 = new BigDecimal(cost2).setScale(4, HALF_UP);
+                averageCost = (cost2 + entryPoint) / 2;
+                BigDecimal resultAverageCost = new BigDecimal(averageCost).setScale(4, HALF_UP);
+
+                amount2 = amount1;
                 exitPrice = 0.95 * averageCost;
-                BigDecimal resultExitPrice = new BigDecimal(exitPrice);
-                resultExitPrice = resultExitPrice.setScale(4, HALF_UP);
+                BigDecimal resultExitPrice = new BigDecimal(exitPrice).setScale(4, HALF_UP);
                 profit = (amount1 * entryPoint + amount2 * cost2) - ((amount2 + amount1) * exitPrice);
-                BigDecimal resultProfit = new BigDecimal(profit);
-                resultProfit = resultProfit.setScale(0, HALF_UP);
+                BigDecimal resultProfit = new BigDecimal(profit).setScale(1, HALF_UP);
 
 
-                cost3 = entryPoint + 0.1 * entryPoint;
+                cost3 = 1.1 * entryPoint;
                 BigDecimal resultCost3 = new BigDecimal(cost3);
                 resultCost3 = resultCost3.setScale(4, HALF_UP);
-                averageCost2 = (cost3 + cost2 + entryPoint) / 3;
-                BigDecimal resultAverageCost2 = new BigDecimal(averageCost2);
-                resultAverageCost2 = resultAverageCost2.setScale(4, HALF_UP);
-                amount3 = (int) floor(640 / cost3);
+                amount3 = (int) ceil((money / entryPoint) * 0.4);
+                averageCost2 = (amount3 * cost3 + amount2 * cost2 + amount1 * entryPoint) / (amount1 + amount2 + amount3);
+                BigDecimal resultAverageCost2 = new BigDecimal(averageCost2).setScale(4, HALF_UP);
                 exitPrice2 = 0.9 * averageCost2;
-                BigDecimal resultExitPrice2 = new BigDecimal(exitPrice2);
-                resultExitPrice2 = resultExitPrice2.setScale(4, HALF_UP);
-                profit2 = (amount1 * entryPoint + amount2 * cost2 + amount3 * cost3) - ((amount3 + amount1 + amount2) * exitPrice);
-                BigDecimal resultProfit2 = new BigDecimal(profit2);
-                resultProfit2 = resultProfit2.setScale(0, HALF_UP);
-                response = ("Монета: " + nameMoney + "\n" + " (Шорт) " +
-                        "1 вход. Кол-во -- " + amount1 + "; Цена покупки: " + entryPoint + "\n" + "\n"
-                        + "2 вход. Кол-во -- " + amount2 + "; Цена покупки: " + resultCost2 + "; Средняя цена: " + resultAverageCost + "\n"
-                        + "Цена выхода (5%): " + resultExitPrice + "; Сумма прибыли: " + resultProfit + "\n" + "\n"
-                        + "3 вход. Кол-во -- " + amount3 + "; Цена покупки: " + resultCost3 + "; Средняя цена: " + resultAverageCost2 + "\n"
-                        + "Цена выхода (10%): " + resultExitPrice2 + "; Сумма прибыли: " + resultProfit2);}
+                BigDecimal resultExitPrice2 = new BigDecimal(exitPrice2).setScale(4, HALF_UP);
+                profit2 = -(exitPrice2 - averageCost2) * (amount1 + amount2 + amount3);
+                BigDecimal resultProfit2 = new BigDecimal(profit2).setScale(1, HALF_UP);
+
+
+                response = ("Монета: " + nameMoney + "(Шорт)" + "\n" +
+                        "\uD83D\uDD34 <b>Точка  входа 1. </b> " + entryPoint +
+                        "\n<b>Размер позиции: </b>" + amount1  + "\n" +
+                        "<b>Точка выхода (3%): </b>" + resultExitPrice1 + "\n" + "\n"
+                        + "\uD83D\uDD34 \uD83D\uDD34 <b> Точка входа 2. </b> " + resultCost2 +
+                        "\n<b>Размер позиции: </b>" + amount2 +  "\n"
+                        + "<b>Точка выхода (5%): </b>" + resultExitPrice + "\n" + "\n"
+                        + "\uD83D\uDD34 \uD83D\uDD34 \uD83D\uDD34 <b> Точка входа 3.</b>" + resultCost3+
+                        "\n<b>Размер позиции: </b>" + amount3 +  "\n"
+                        + "<b>Точка выхода (10%): </b>" + resultExitPrice2);
             }
-         else {
+
+        } else {
             response = "Введи название на английском или используй точку при вводе числа";
         }
         return response;
     }
+
+
+
+
+
+
+
+
 
 
     private boolean isNumeric(String str) {
@@ -152,7 +177,9 @@ public class Bot extends TelegramLongPollingBot {
             return false;
         }
     }
-    // Метод создания кнопок "Лонг" и "Шорт"
+
+
+    // Метод создания кнопок "Лонг" и "Шорт" и "Конвертор валют"
     public static ReplyKeyboardMarkup getOperationKeyboard() {
         ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
         List<KeyboardRow> keyboard = new ArrayList<>();
@@ -174,6 +201,6 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "6270969692:AAFSdIfDGtnD8bB-xWsM2rKKGZ67-Z6nDok";
+        return "6270969692:AAElvvioZSRGftnZfCSAVHWf9c4gZ9U-umY";
     }
 }
